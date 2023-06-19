e31efaedecb1be55d24bb6a9e7bedef3acff78c8
gayathri-mg3 <137045127+gayathri-mg3@users.noreply.github.com>
2023-06-19 23:10:05 +0530

    Add files via upload

---
+++ day1/Assignment1.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class Assignment1 {
+	public void actiodriverCar() {
+		System.out.println("Drive Car");
+	}
+	public void actionapplyBreak() {
+		System.out.println("Apply Break");
+	}
+	public void actionsoundHorn() {
+		System.out.println("Sound Horn");
+	}
+	public void actionisPuncture() {
+		System.out.println("is Puncture");
+	}
+	public static void main(String[] args) {
+		Assignment1 car=new Assignment1();
+		car.actiodriverCar();
+		car.actionapplyBreak();
+		car.actionsoundHorn();
+		car.actionisPuncture();
+		
+		
+		
+	}
+
+}
---
+++ day1/iPhone.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class iPhone {
+	public void actionmakeCall() {
+		System.out.println("Make a Call");
+	}
+	public void actioncheckmodel() {
+		System.out.println("Iphone 13 Mini");
+	}
+
+}
